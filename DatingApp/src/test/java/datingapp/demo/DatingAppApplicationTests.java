a6a8b5505549eb4132b3407e98f7e2112f8a72af
Kameloozo <64187942+Kameloozo@users.noreply.github.com>
2020-11-02 14:07:39 +0100

    tilføjet projekt

---
+++ DatingApp/src/test/java/datingapp/demo/DatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package datingapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

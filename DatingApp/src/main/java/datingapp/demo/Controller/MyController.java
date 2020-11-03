6b025ebdac37f572000b64434faf323533b7e692
Thomas Simonsen <thom622g@stud.kea.dk>
2020-11-03 13:02:13 +0100

    tilføjet static og templates mapper samt lavet en frontPage.html fil

---
+++ DatingApp/src/main/java/datingapp/demo/Controller/MyController.java
@@ -0,0 +1,15 @@
+package datingapp.demo.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "frontPage";
+    }
+
+}
